373e84fc9f186e1b88214fe866616e1a2b63cb24
Miguel Gonzalez <miguel.gonzalez@sckans.edu>
2022-09-21 08:46:15 -0500

    .\oddNumber.py

--- oddNumber.py
+++ oddNumber.py
@@ -4,7 +4,10 @@
 # Last Modified: 09/21/2022
 # Version : 3.7.9
 
-print("Printing odd numbers from the first 100 natural numbers")
-for i in range(0,101):
-    if(i%2 !=0):
-        print(i, end="")
+# print("Printing odd numbers from the first 100 natural numbers")
+# for i in range(0,101):
+#     if(i%2 !=0):
+#         print(i, end=" ")
+
+for k in range (1,101):
+    print((2*k)-1, end=" ")
