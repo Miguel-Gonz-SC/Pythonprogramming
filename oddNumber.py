031ec0492ded1aee51aa65c4a9b421c1df8d7d8a
Miguel Gonzalez <miguel.gonzalez@sckans.edu>
2022-09-21 08:27:53 -0500

    .\oddNumber.py

---
+++ oddNumber.py
@@ -0,0 +1,10 @@
+# Programmer: Miguel Gonzalez
+# Program: oddNumber
+# Created on: 09/21/2022
+# Last Modified: 09/21/2022
+# Version : 3.7.9
+
+print("Printing odd numbers from the first 100 natural numbers")
+for i in range(0,101):
+    if(i%2 !=0):
+        print(i, end="")
