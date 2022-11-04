be5b54469b93fe013fa06111839c0b6786436e63
Miguel Gonzalez <miguel.gonzalez@sckans.edu>
2022-11-04 09:18:24 -0500

    .\README.md

--- Final Proj Prog/README.md
+++ Final Proj Prog/README.md
@@ -1,1 +1,17 @@
-# Pythonprogramming
+# Car-Sales
+<p>This code is helping people to sell cars more
+quickly and efficiently while showing how many care
+are left over at the end of the day.</p>
+
+## Team Members
+**1. Zach Hidalgo**
+
+**2. Miguel Gonzalez**
+
+## Software: 
+The required software / module versions are:
+
+1. Python : 3.10.8
+2. Pandas 
+3. Math
+4. Numpy
--- Final Proj Prog/carSales.py
+++ Final Proj Prog/carSales.py
@@ -9,7 +9,26 @@ Last modified: 10/31/2022 8:27:00
 print("What make of car are you looking for")
 print("We have Trucks, Sedans, Coupes, Hatchbacks, Convertibles, SUV's, and Vans")
 
-P = input("  \t")
+P = int(input("Trucks < 1, Sedans < 2, Coupes < 3, Hatchbacks < 4, Convertibles < 5, SUV's < 6 , Vans < 7 \t"))
 
-if P = Trucks
-
+if P == 1:
+    g = open("inputFile")
+    print(g.read())
+elif P == 2:
+    g = open("inputFile")
+    print(g.read())
+elif P == 3:
+    g = open("inputFile")
+    print(g.read())
+elif P == 4:
+    g = open("inputFile")
+    print(g.read())
+elif P == 5:
+    g = open("inputFile")
+    print(g.read())
+elif P == 6:
+    g = open("inputFile")
+    print(g.read())
+elif P == 7:
+    g = open("inputFile")
+    print(g.read())
